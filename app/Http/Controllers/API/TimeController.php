794e3155b1d34e09f82aa070ba1039238970902f
Colin-Kapsner <colinkapsner@yahoo.com>
2024-04-25 22:57:32 -0600

    .

--- app/Http/Controllers/API/TimeController.php
+++ app/Http/Controllers/API/TimeController.php
@@ -32,7 +32,7 @@ class TimeController extends Controller
         $times = Time::query()
                 ->where('time', '>', 9)
                 ->orderBy('time', 'asc')
-                ->groupBy('user_id')
+                ->distinct()
                 ->limit(10)
                 ->get();
         
