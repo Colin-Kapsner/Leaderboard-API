e2b4990771c5a9b276c754086bf66100a866bf02
Colin-Kapsner <colinkapsner@yahoo.com>
2024-04-25 22:29:00 -0600

    .

--- app/Http/Controllers/API/TimeController.php
+++ app/Http/Controllers/API/TimeController.php
@@ -29,9 +29,10 @@ class TimeController extends Controller
     public function allTopTimes(Request $request)
     {
 
-        $times = Time::query()
+        $times = Time::select('time')
                 ->where('time', '>', 9)
                 ->orderBy('time', 'asc')
+                ->groupBy('user_id')
                 ->limit(10)
                 ->get();
         return new TimesResource($times);
